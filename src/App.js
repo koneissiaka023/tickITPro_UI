0a387272098ab69b85218efa63bf525dd77defd7
calebabeck2020 <calebabeck@gmail.com>
2022-09-08 19:32:59 -0400

    fixing merge conflicts

--- src/App.js
+++ src/App.js
@@ -7,13 +7,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-<<<<<<< HEAD
-<p>yeah its sam</p>    </div>
-=======
-<<<<<<< HEAD
-      
-=======
       <BrowserRouter>
         <Provider store={store}>
           <Navbar />
@@ -22,12 +15,7 @@ function App() {
           </Routes>
         </Provider>
       </BrowserRouter>
->>>>>>> 30bb0b5303c76aa65bd563dee0af647ab3c5ff4a
-=======
-
->>>>>>> db1b7469beed48c7e04a15fbbbc3f8a47dedaec5
     </div>
->>>>>>> b3dcf08c11d582ff2f369472628a76ff2eb37a5b
   );
 }
 

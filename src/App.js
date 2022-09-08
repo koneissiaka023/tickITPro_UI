feae37c0904ca460d013045b798ecb4b3a92fe4d
calebabeck2020 <calebabeck@gmail.com>
2022-09-08 17:27:41 -0400

    adjusted App.js, added client, addAuthHeader, and store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Provider } from "react-redux";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import LandingPage from "./components/LandingPage/landingpage";
+import Navbar from "./components/LandingPage/navbar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Provider store={store}>
+          <Navbar />
+          <Routes>
+            <Route exact path="" element={<LandingPage />} />
+          </Routes>
+        </Provider>
+      </BrowserRouter>
     </div>
   );
 }

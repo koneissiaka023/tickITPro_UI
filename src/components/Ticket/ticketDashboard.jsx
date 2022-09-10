98ec3c03dc0a94c845a7987d6f2ce32985208418
koneissiaka023 <koneissiaka023@gmail.com>
2022-09-09 21:30:52 -0400

    Cleaning up the code

--- src/components/Ticket/ticketDashboard.jsx
+++ src/components/Ticket/ticketDashboard.jsx
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 import AddTicketToPool from "./createTicket"
 import TicketTable from "./tickets"
 
-=======
->>>>>>> 28ebd295f361b4bd145931357f3faa0e51cb76eb
 export default function TicketDashboard(){
     const ticketId = useSelector((state) => state.loginSlice.ticketId)
 
     return(
-<<<<<<< HEAD
-=======
 
->>>>>>> 28ebd295f361b4bd145931357f3faa0e51cb76eb
         <>
             <h1>{ticketId} Ticket Dashboard</h1>
             <p>

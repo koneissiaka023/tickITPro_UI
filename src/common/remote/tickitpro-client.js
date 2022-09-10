34873fe6b1640155c3c8b7ef00d9f77d27c14e04
Byron <byronfedele@gmail.com>
2022-09-09 22:22:27 -0400

    Changed tickitpro-client to web url for testing

--- src/common/remote/tickitpro-client.js
+++ src/common/remote/tickitpro-client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const tickITProClient = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "http://tickitpro-env.eba-b8m8caz3.us-east-1.elasticbeanstalk.com/",
     headers: {
         Accept: "application/json",
         "Content-type": "application/json",

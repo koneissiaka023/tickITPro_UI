28ebd295f361b4bd145931357f3faa0e51cb76eb
koneissiaka023 <koneissiaka023@gmail.com>
2022-09-09 20:55:54 -0400

    added logout function (not functional yet)

--- src/components/Ticket/ticketDashboard.jsx
+++ src/components/Ticket/ticketDashboard.jsx
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-export default function TicketDashboard() {
-
-
-
-    
-    return (
-=======
 export default function TicketDashboard(){
     const ticketId = useSelector((state) => state.loginSlice.ticketId)
 
     return(
->>>>>>> b2a8571a6428c6743c2c9708365f5c5c603daf2f
+
         <>
             <h1>{ticketId} Ticket Dashboard</h1>
             <p>

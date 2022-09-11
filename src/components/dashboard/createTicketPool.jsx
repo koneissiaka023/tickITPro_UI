dd1061de2917a57d47a0c5f184ed57ea9df306a1
sbox101 <56304767+sbox101@users.noreply.github.com>
2022-09-11 12:29:39 -0400

    file structure outline for departments

---
+++ src/components/dashboard/createTicketPool.jsx
@@ -0,0 +1,92 @@
+import { createContext, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import AuthCheck from "../../common/authCheck/authCheck";
+import addAuthToken from "../../common/remote/addAuthHeader";
+import { tickITProClient } from "../../common/remote/tickitpro-client";
+import AddTicketToPool from "./addTicketToPool";
+import { sendTicket } from "./ticketPoolSlice";
+
+export const ticketCreationContext = createContext();
+
+export default function CreateTicketPool() {
+    {/* AuthCheck(false); */}
+    const email = useSelector((state) => state.loginSlice.email);
+    const [message, setMessage] = useState();
+
+    {/* const tickets = useSelector((state) => state.AddTicketToPool); */}
+
+    const [formData, setFormData] = useState({
+        description: "",
+        priority: "DEFAULT",
+        subjectId: ""
+    });
+    const [submit, setSubmit] = useState(true);
+
+    function defaultSub(e) {
+        e.preventDefault();
+    }
+
+    async function submitTicket(e) {
+        e.preventDefault();
+        try {
+            addAuthToken();
+            console.log(formData);
+            const response = await tickITProClient.post("/ticket", formData);
+            console.log(response.data);
+            setMessage(`Ticket has been successfully submitted! ${response.data.ticketId}`);
+        } catch (error) {
+            console.log(error.response.data);
+
+            if(error.response.status === 400){
+                setMessage(`Could not create ticket: ${error.response.data}`);
+            }
+        }
+    }
+
+    const formFunctions = {
+        description: function (event) {
+            setFormData({ ...formData, description: event.target.value });
+        },
+        priority: function (event) {
+            setFormData({ ...formData, priority: event.target.value });
+        },
+        subjectId: function (event) {
+            setFormData({ ...formData, subjectId: event.target.value });
+        }
+    };
+
+    return (
+        <>
+
+            <form onSubmit={defaultSub}>
+                <label>Description:</label>
+                <textarea class="ticket" placeholder="i.e describe your issue" onChange={formFunctions.description} />
+                <br />
+
+                <div>
+                    <label>Priority:</label>
+                    <input type="radio" id="techRadio1" name="priority" value="DEFAULT" onChange={formFunctions.priority} />
+                    <label>DEFAULT</label>
+                    <input type="radio" id="techRadio2" name="priority" value="LOW_PRIORITY" onChange={formFunctions.priority} />
+                    <label>LOW_PRIORITY</label>
+                    <input type="radio" id="techRadio3" name="priority" value="HIGH_PRIORITY" onChange={formFunctions.priority} />
+                    <label>HIGH_PRIORITY</label>
+                </div>
+
+                <label>SubjectId:</label>
+                <textarea class="ticket" placeholder="i.e enter subject ID" onChange={formFunctions.subjectId} />
+                <ticketCreationContext.Provider value={[formData,setFormData]}>
+                    {/*<SubjectDropDown />*/}
+                </ticketCreationContext.Provider>
+                <br />
+
+                <input type="hidden" id="prioritySelect" class="ticket" value=""></input>
+
+                {<AddTicketToPool ticket={formData} /> }
+            </form>
+
+            <button onClick={submitTicket}>Submit Ticket</button>
+            
+        </>
+    );
+}

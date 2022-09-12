8ce2c63186ff16e57c063eed1364c932d229921c
sbox101 <56304767+sbox101@users.noreply.github.com>
2022-09-11 21:09:23 -0400

    added edit and admit editor for departments

--- src/components/Department/editDepartment.jsx
+++ src/components/Department/editDepartment.jsx
@@ -1,20 +1,79 @@
 import DepartmentDropDown from "./departmentDropDown"
-import {createContext, useState } from "react"
+import {createContext, useContext, useState } from "react"
 import { departmentDropdownContext } from "../../App"
+import addAuthToken from "../../common/remote/addAuthHeader"
+import { tickITProClient } from "../../common/remote/tickitpro-client"
+import { subjectEditorContext } from "../Subject/editSubject"
 
 
 export const departmentEditorContext = createContext()
 
 export default function EditDepartment(){
-    const [formData, setFormData] = useState()
+    const [creation,setCreation] = useContext(subjectEditorContext)
+
+    const [formData,setFormData] = useState({
+        id: "",
+        name: ""
+    });
+
+    async function createDepartment(d) {
+        d.preventDefault();
+        try {
+            addAuthToken();
+            console.log(formData);
+            const response = await tickITProClient.post("/department",formData);
+            console.log(response.data);
+            setCreation(`Department has been successfully created! ${response.data.name}`);
+        } catch (error) {
+            console.log(error.response.data);
+        }
+    }
+
+    async function updateDepartment(d){
+        d.preventDefault()
+        try{
+            addAuthToken()
+            console.log(formData)
+            const response = await tickITProClient.post("/department", formData)
+            console.log(response.data)
+            setCreation(`Department successfully created ${response.data.name}`)
+        } catch(error){
+            console.log(error.response.data)
+        }      
+    }
+
+    async function deleteDepartment (d) {
+        d.preventDefault()
+        try {
+            addAuthToken()
+            console.log(formData)
+            const response = await tickITProClient.post("/department", formData)
+            console.log(response.data)
+            setCreation(`Department successfully deleted ${response.data.name}`)
+        } catch(error){
+            console.log(error.response.data)
+        }      
+}
+    
+
 
     return (
         <>
-            <departmentEditorContext.Provider>
+            <form><departmentEditorContext.Provider>
                 <departmentDropdownContext.Provider value = {{formData, setFormData}}>
                     <DepartmentDropDown></DepartmentDropDown>
                 </departmentDropdownContext.Provider>
             </departmentEditorContext.Provider>
+            <input 
+                    placeholder="Department Name here"
+                    onChange = {(e) => {
+                        setFormData({...formData, name: e.target.value});
+                    }} />
+                <button onClick={updateDepartment}>Update Department</button>
+                <button onClick={createDepartment}>Create Department</button>
+                <button onClick={deleteDepartment}>Delete Department</button>
+            </form>
+            <p>{creation}</p>
         </>
     )
 }
